5a17bf7d17458a98207428245960ece5555b6947
hokcung <fonghokcung@139.com>
2016-12-07 10:46:07 +0800

    function style runjs

---
+++ data.php
@@ -0,0 +1,4 @@
+<?php
+$data='{"name":"try","version":"1.0.0","description":"learning","main":"index.js","dependencies":{"gulp-html-validator":"file:gulp-html-validator"},"devDependencies":{"gulp":"^3.9.1","gulp-clean-css":"^2.0.8","gulp-csscomb":"^3.0.7","gulp-html-prettify":"0.0.1","gulp-htmlhint":"^0.3.1","gulp-htmlmin":"^2.0.0"},"scripts":{"test":"echo \"Error: no test specified\" && exit 1"},"author":"","license":"ISC"}';
+echo $data;
+?>
